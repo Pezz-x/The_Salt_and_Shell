888c4c41b849e4ecd5838d960d8c160a91657d2f
JoshThould <jhethould@gmail.com>
2025-08-14 11:48:55 +0100

    ADD: Define Customer model with name, email, and phone number fields

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+    return f"{self.name} ({self.email}, {self.phone_number})"
+
