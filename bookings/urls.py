6e8e16e268baac7d484a47b8b7b469ef1c7737b9
JoshThould <jhethould@gmail.com>
2025-08-14 13:49:57 +0100

    ADD: Implement booking views and update URL routing for booking management

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bookings_page, name='bookings'),
+    path('', BookingListView.as_view(), name='booking-list'),
+    path('<int:pk>/', BookingDetailView.as_view(), name='booking-detail'),
+    path('create/', BookingCreateView.as_view(), name='booking-create'),
+    path('<int:pk>/edit/', BookingUpdateView.as_view(), name='booking-update'),
+    path('<int:pk>/delete/', BookingDeleteView.as_view(), name='booking-delete'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Booking
 
 # Create your views here.
-def bookings_page(request):
-    """View for the bookings page"""
-    return render(request, 'bookings.html')
+
+class BookingListView(ListView):
+    model = Booking
+    template_name = 'bookings/booking_list.html'
+    context_object_name = 'bookings'
+
+class BookingDetailView(DetailView):
+    model = Booking
+    template_name = 'bookings/booking_detail.html'
+
+class BookingCreateView(CreateView):
+    model = Booking
+    fields = ['customer', 'time_slot_id', 'party_size', 'special_requests']
+    template_name = 'bookings/booking_form.html'
+    success_url = reverse_lazy('booking-list')
+
+class BookingUpdateView(UpdateView):
+    model = Booking
+    fields = ['customer', 'time_slot_id', 'party_size', 'special_requests']
+    template_name = 'bookings/booking_form.html'
+    success_url = reverse_lazy('booking-list')
+
+class BookingDeleteView(DeleteView):
+    model = Booking
+    template_name = 'bookings/booking_confirm_delete.html'
+    success_url = reverse_lazy('booking-list')
