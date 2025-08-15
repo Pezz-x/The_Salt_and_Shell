b942897ba1b0b9356c7d8e2a0b33b6314584994d
Pezz-x <pyranfisher@gmail.com>
2025-08-15 16:59:51 +0200

    Revert "altered time slots displaying"
    This reverts commit c3d0b9e082297bd16d3cdb2bf10550cae8cb5571.

--- bookings/forms.py
+++ bookings/forms.py
@@ -7,7 +7,7 @@ class BookingForm(forms.ModelForm):
     customer_email = forms.EmailField()
     customer_phone = forms.CharField(max_length=20)
     date = forms.DateField(widget=forms.SelectDateWidget)
-    time_slot = forms.ModelChoiceField(queryset=TimeSlot.objects.all())
+    time_slot = forms.ModelChoiceField(queryset=TimeSlot.objects.none())
 
     class Meta:
         model = Booking
@@ -19,22 +19,15 @@ class BookingForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Default: show all available slots
-        self.fields['time_slot'].queryset = TimeSlot.objects.filter(is_available=True)
-
-        # If user picked a date, filter down further
+        # If date is pre-filled, filter slots for that date
         if 'date' in self.data:
             try:
-                from datetime import datetime
                 date_val = self.data.get('date')
+                # date_val is "YYYY-MM-DD"
+                from datetime import datetime
                 parsed_date = datetime.strptime(date_val, "%Y-%m-%d").date()
-                self.fields['time_slot'].queryset = TimeSlot.objects.filter(
-                    start_time__date=parsed_date,
-                    is_available=True
-                )
+                self.fields['time_slot'].queryset = TimeSlot.objects.filter(start_time__date=parsed_date, is_available=True)
             except ValueError:
                 pass
         elif self.instance.pk:
-            self.fields['time_slot'].queryset = TimeSlot.objects.filter(
-                start_time__date=self.instance.time_slot.start_time.date()
-            )
+            self.fields['time_slot'].queryset = TimeSlot.objects.filter(start_time__date=self.instance.time_slot.start_time.date())
